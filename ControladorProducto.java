d16167fd00e310e18076771ab336cc96b984504d
Miguel2436 <mi24guel36@gmail.com>
2018-06-20 22:04:26 -0500

    Transaccion, Producto
    producto: Se agregó el método 'misProductos' junto con el método 'setUsuarios'.
    Transaccion: Se agregó el método 'listaInteresados' el cual retorna una lista de los registros de la tabla 'transaccion' cuya FKproducto sea igual al atributo 'productoId' del controlador. (Tambien está el método setProductoId)

---
+++ ControladorProducto.java
@@ -0,0 +1,106 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Controladores;
+
+import Entities.Categoria;
+import Entities.Producto;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Iterator;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import javax.persistence.Query;
+
+
+
+/**
+ *
+ * @author Leonardo Martinez
+ */
+@Named("controladorProducto")
+@SessionScoped
+public class ControladorProducto implements Serializable{
+    private Producto producto;
+    private String categoriaAMostrar = "Todas";
+      private String usuario="";
+
+    @EJB
+    private FachadaProducto fachada;
+    
+    public ControladorProducto() {
+        
+    }
+    
+    public FachadaProducto getFachada() {
+        return fachada;
+    }
+    
+    public void setCategoriaAMostrar(String categoria) {
+        categoriaAMostrar = categoria;
+    }
+    
+    public Producto getProducto() {
+        if (producto == null) {
+            producto = new Producto();
+        }
+        return producto;
+    }
+    
+    public void crearProducto() {
+        getFachada().create(producto);
+    }
+    
+    public List<Producto> getProductos() {
+        List<Producto> productos = getFachada().findAll();
+        if (categoriaAMostrar.equals("Todas")) {
+            return productos;
+        } else {
+            Query consultaPkCategoria = getFachada().getEntityManager().createQuery("Select a.pkcategoria From Categoria a Where a.nombre = :categoria");
+            consultaPkCategoria.setParameter("categoria", categoriaAMostrar);
+            int fkCategoria = (int)consultaPkCategoria.getSingleResult();
+            
+            Query consulta = getFachada().getEntityManager().createQuery("Select a From Producto a Where a.fkcategoria = :categoria");
+            consulta.setParameter("categoria", fkCategoria);
+            
+            return (List<Producto>)consulta.getResultList();
+        }
+    }
+    public Producto findProducto(int pk)
+    {
+        return getFachada().find(pk);
+    }
+    public String convertImg(int pk)
+    {
+        Producto producto =getFachada().find(pk);
+        byte[] photo = producto.getImagen();
+        String bphoto = Base64.getEncoder().encodeToString(photo);
+        return bphoto;
+    }
+
+
+    public void setUsuario(String correo){
+        this.usuario=correo;
+    }
+    
+    public List<Producto> getMisProductos(){   
+        List<Producto> misProductos = new ArrayList<>();
+        String correo;
+        List<Producto> productos = getFachada().findAll();    
+        for(int i=0;i<productos.size();i++){
+        correo=productos.get(i).getFkusuario().getCorreo();
+            if(correo.equals(this.usuario))
+            misProductos.add(productos.get(i));
+        }
+
+        return misProductos;
+    }
+
+}
---
+++ ControladorTransaccion.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controladores;
+
+import Entities.Transaccion;
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.util.ArrayList;
+/**
+ *
+ * @author Leonardo Martinez
+ */
+@Named("controladorTransaccion")
+@SessionScoped
+public class ControladorTransaccion implements Serializable{
+    private Transaccion transaccion;
+    private int productoId;
+    @EJB
+    private FachadaTransaccion fachada;
+    
+    public ControladorTransaccion() {
+        
+    }
+    
+    public FachadaTransaccion getFachada() {
+        return fachada;
+    }
+    
+    public Transaccion getTransaccion() {
+        if (transaccion == null) {
+            transaccion = new Transaccion();
+        }
+        return transaccion;
+    }
+    
+    public void crearTransaccion() {
+        getFachada().create(transaccion);
+    }
+    
+    public List<Transaccion> getTransaccions() {
+        return getFachada().findAll();
+    }
+
+    public void setProductoId(int productoId){
+        this.productoId=productoId;
+    }
+    
+    public List<Transaccion> ListaInteresados(){
+       
+    List<Transaccion> listaDeInteresados = new ArrayList<>();
+    int producto;
+    String correo;
+    List<Transaccion> transacciones = getFachada().findAll();    
+    for(int i=0;i<transacciones.size();i++){
+    producto=transacciones.get(i).getFkproducto().getPkproducto();
+        if(producto==this.productoId)
+        listaDeInteresados.add(transacciones.get(i));
+    }
+    return listaDeInteresados;
+    }
+    
+    public int getCantidadTransaccion(int pk){
+        int cantidad = 6, numero = 0;
+        
+        /*ControladorProducto controladorProducto = new ControladorProducto();
+        Iterator<Transaccion> itrTransaccion = getTransaccions().iterator();
+        while (itrTransaccion.hasNext()) {
+            Transaccion temp = itrTransaccion.next();
+            numero = temp.getFkproducto().getPkproducto();
+            if(temp.getElegido()){
+                Producto producto = controladorProducto.getFachada().findAll().get(temp.getFkproducto().getPkproducto());
+                if (producto.getFkcategoria().getPkcategoria() == pk) {
+                    cantidad++;
+                }
+                //numero = producto.getFkcategoria().getPkcategoria();
+                cantidad++;
+            }
+        }*/
+        return cantidad;
+    }
+    
+}
